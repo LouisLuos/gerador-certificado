778c93cc1148c1ec0f7c0d57047393ccc9d66f32
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-09-24 16:16:56 -0300

    feat; pagina de sem certificados

--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
+import { ItemCertificado } from "../../_components/item-certificado/item-certificado";
 
 @Component({
   selector: 'app-certificados',
-  imports: [PrimaryButton, SecondaryButton],
+  imports: [SecondaryButton, ItemCertificado],
   templateUrl: './certificados.html',
   styleUrl: './certificados.css'
 })
